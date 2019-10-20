1220ece1700febf073294fca910f2311ebe829b8
tomporvaz <tomporvaz@gmail.com>
2019-10-20 18:20:20 -0400

    Added helmet.js middleware and setup policies.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var cors        = require('cors');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+const helmet = require('helmet');
 
 var app = express();
 
@@ -17,6 +18,13 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//set security headers with helmet
+app.use(helmet({
+  frameguard: {action: 'sameorigin'},
+  referrerPolicy: {policy: 'same-origin'}
+}));
+
 
 //Sample front-end
 app.route('/b/:board/')
